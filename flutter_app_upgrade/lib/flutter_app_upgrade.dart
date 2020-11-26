3c032af40405e280318cf07656941602506edb3f
梦伟褚 <cmw_tianfa@foxmail.com>
2020-11-26 11:43:15 +0800

    增加export文件

--- flutter_app_upgrade/lib/flutter_app_upgrade.dart
+++ flutter_app_upgrade/lib/flutter_app_upgrade.dart
@@ -6,3 +6,5 @@ export 'src/app_upgrade.dart';
 export 'src/flutter_upgrade.dart';
 export 'src/app_market.dart';
 export 'src/download_status.dart';
+export 'src/simple_app_upgrade.dart';
+export 'src/liquid_progress_indicator.dart';
